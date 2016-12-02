a8032acce85f237892f71a117fccf820a5d716c4
Maali Tars <tarsmaali@gmail.com>
2016-12-02 14:58:05 +0200

    Structure for sentiment analysis.

---
+++ sentiment.py
@@ -0,0 +1,40 @@
+from collections import defaultdict
+
+def loe_korpus(algkaust):
+    tweedid = []
+    ###for i in algkaust:
+    ###     if i.isfile:
+    ###         puhastatud = puhasta(i.read())
+    ###         tweedid.append(puhastatud)
+    return tweedid
+
+def sonesta(tweedid):
+    sonestatud = []
+    ##for i in tweedid:
+    ##      sonestatud.append(sõnesta i)
+    ##
+    return sonestatud
+
+def leia_sagedused(treening_korpus):
+    sagedused = defaultdict(int)
+    #treeningkorpus on list, mis sisaldab endas sõnestatud
+    #tweete, mis on samuti listi kujul, ehk kokkuvõttes on meil
+    #listide list.
+    return sagedused
+
+def leia_enim_unikaalsed(sagedused):
+    unikaalsed = []
+    return unikaalsed
+
+def silumine():
+
+    return None
+
+
+def testimine():
+
+    return None
+
+def main():
+
+    print("jou")
