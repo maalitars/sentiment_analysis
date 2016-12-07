d10f4fd6f95c32902c2b4afd66af9db92094c00e
maalitars <tarsmaali@gmail.com>
2016-12-07 18:16:34 +0200

    Delete sentiment_lugemine.py

--- sentiment_lugemine.py
+++
@@ -1,5 +0,0 @@
-f = open('tweedid.txt',encoding='UTF-8')
-for rida in f:
-    rida.strip()
-    lahku = rida.split('","') #praegu on esimesel sõnal ees jutumärk ja tweedi lõpus on üks jutumärk"
-    print(lahku)
